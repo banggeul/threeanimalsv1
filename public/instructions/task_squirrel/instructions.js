5c7601f780c9883bf4b9b4c4605b7419d59e8fd1
Coventry <coventry@coventrys-mbp.mynetworksettings.com>
2023-04-03 13:36:22 -0400

    new quirrel assets

--- public/instructions/task_squirrel/instructions.js
+++ public/instructions/task_squirrel/instructions.js
@@ -3,7 +3,7 @@ export var instructions = [
     foreground: "big_squirrel.png",
     isVideo: "0",
     text: {
-      en:"This is Lucy, Ollie and Skipper.",
+      en:"This is Lucy, Ollie and Skipper. <br><br> You will be guessing what they may find <br> as they hop along the path.",
       de:"Das ist Maxi. <br><br> Maxi ist sehr hungrig und Karotten <br>sind Maxis Lieblingsessen. <br><br>Maxi sucht nach Karotten zum Essen."
     },
     textDelay: "1",
@@ -18,11 +18,11 @@ export var instructions = [
     foreground: "big_squirrel.png",
     isVideo: "0",
     text: {
-      en:"This is Maxi. <br><br> Maxi is very hungry and carrots <br>are Maxi’s favorite food. <br><br>Maxi is looking for carrots to eat.",
+      en:"This is Skipper. <br><br> Skipper is very hungry and acorns <br>are Skipper’s favorite food. <br><br>Skipper is looking for acorns to eat.",
       de:"Das ist Maxi. <br><br> Maxi ist sehr hungrig und Karotten <br>sind Maxis Lieblingsessen. <br><br>Maxi sucht nach Karotten zum Essen."
     },
     textDelay: "1",
-    x:"45%",
+    x:"60%",
     y:"40%",
     image_x: "5%",
     image_y: "0",
@@ -39,7 +39,7 @@ export var instructions = [
       duration: "3"
     },
     text: {
-      en: "See these spots? <br> Maxi will follow this path looking for carrots in each spot.",
+      en: "See these spots? <br> Skipper will follow this path looking for acorns in each spot.",
       de:"Siehst du diese Punkte? <br>Maxi folgt diesem Pfad und sucht an jedem Punkt nach Karotten."
     },
     textDelay: "2",
@@ -56,7 +56,7 @@ export var instructions = [
     foreground: "carrot_flip.mp4",
     isVideo: "1",
     text: {
-      en:"Under some of the spots, Maxi can find <br> a yummy carrot that looks like this…",
+      en:"Under some of the spots, Skipper can find <br> a yummy acorn that looks like this…",
       de:"Unter einigen Punkten findet Maxi  <br> eine leckere Karotte, die so aussieht…"
     },
     textDelay: "0",
@@ -95,7 +95,7 @@ export var instructions = [
     },
     isVideo: "0",
     text: {
-      en:"Maxi knows that it is as likely to <br> find a carrot under a spot as it is to <br>find nothing under a spot.",
+      en:"Skipper knows that it is as likely to <br> find an acorn under a spot as it is to <br>find nothing under a spot.",
       de:"Maxi weiß, dass es genauso gut möglich ist, <br> eine Karotte unter einem Punkt zu finden,  <br>wie es möglich ist, keine Karotte zu finden."
     },
     textDelay: "0",
@@ -117,7 +117,7 @@ export var instructions = [
     },
     isVideo: "0",
     text: {
-      en:"Let’s help Maxi guess <br>if there is something to eat <br> or nothing to eat under each spot.",
+      en:"Let’s help Skipper guess <br>if there is something to eat <br> or nothing to eat under each spot.",
       de:"Hilf Maxi zu erraten, <br> ob es unter den einzelnen Punkten <br>etwas zu essen oder nichts zu essen gibt."
     },
     textDelay: "1",
